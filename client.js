cd82d1a3f65366982e67fc776535a381a55b0287
unknown <ttosi@DEVELOPER03.arminsight.local>
2015-08-19 10:20:07 -0700

    Add command line arguments

--- client.js
+++ client.js
@@ -1,13 +1,24 @@
 var net = require('net'),
-	publicIp = require('public-ip')
+	publicIp = require('public-ip'),
+	args = require("command-line-args"),
 	currentIp = '';
+	
+var options = args([
+	{ name: "server", alias: "s", type: String },
+	{ name: "port", alias: "p", type: Number, defaultValue: 1337 },
+	{ name: "interval", alias: "i", type: Number, defaultValue: 300000 }
+]).parse();
+
+if(!options.server) {
+	console.log('ipupdater server address required: --server, -s <server address>');
+	return false;
+}
 
 setInterval(function() {
 	publicIp(function (err, ip) {
 		if(ip !== currentIp) {
 			var client = new net.Socket();
-			
-			client.connect(1337, 'tdc2.turningdigital.com', function() {
+			client.connect(options.port, options.server, function() {
 				client.write(ip);
 				currentIp = ip;
 
@@ -15,5 +26,5 @@ setInterval(function() {
 			});
 		}
 	});
-}, 30000);
+}, options.interval);
 
