34a2e0ff65f4abea6ec6a7fe9fd0687216c1791f
unknown <ttosi@DEVELOPER03.arminsight.local>
2015-08-19 10:29:22 -0700

    Tabs to spaces

--- client.js
+++ client.js
@@ -1,30 +1,30 @@
 var net = require('net'),
-	publicIp = require('public-ip'),
-	args = require("command-line-args"),
-	currentIp = '';
-	
+    publicIp = require('public-ip'),
+    args = require("command-line-args"),
+    currentIp = '';
+    
 var options = args([
-	{ name: "server", alias: "s", type: String },
-	{ name: "port", alias: "p", type: Number, defaultValue: 1337 },
-	{ name: "interval", alias: "i", type: Number, defaultValue: 300000 }
+    { name: "server", alias: "s", type: String },
+    { name: "port", alias: "p", type: Number, defaultValue: 1337 },
+    { name: "interval", alias: "i", type: Number, defaultValue: 300000 }
 ]).parse();
 
 if(!options.server) {
-	console.log('ipupdater server address required: --server, -s <server address>');
-	return false;
+    console.log('ipupdater server address required: --server, -s <server address>');
+    return false;
 }
 
-setInterval(function() {
-	publicIp(function (err, ip) {
-		if(ip !== currentIp) {
-			var client = new net.Socket();
-			client.connect(options.port, options.server, function() {
+setInterval(function () {
+    publicIp(function (err, ip) {
+        if(ip !== currentIp) {
+            var client = new net.Socket();
+            client.connect(options.port, options.server, function () {
 				client.write(ip);
-				currentIp = ip;
+                currentIp = ip;
 
-				client.destroy();
-			});
-		}
-	});
+                client.destroy();
+            });
+        }
+    });
 }, options.interval);
 
