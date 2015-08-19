eae80b374e77f2ca2c402d1a51d85dd266eb67f4
ttosi <ttosi519@gmail.com>
2015-08-19 11:47:12 -0700

    Minor formatting cleanup

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 var net = require('net'),
     http = require('http'),
     args = require('command-line-args'),
-	moment = require('moment');
+    moment = require('moment');
     
 var options = args([
     { name: 'tcp-port', alias: 't', type: Number, defaultValue: 1337 },
@@ -27,10 +27,9 @@ var options = args([
 
 var server = net.createServer(function (socket) {
     var currentIp;
-	
-	socket.on('data', function (data) {
+    socket.on('data', function (data) {
         var timestamp = moment(new Date()).format('MM-DD-YYYY hh:mm:ss A');
-		currentIp = data + ' (updated ' + timestamp + ')';
+        currentIp = data + ' (updated ' + timestamp + ')';
     });
 }).listen(options['tcp-port']);
 
