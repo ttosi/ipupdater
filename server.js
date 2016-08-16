103f7a95b6e980e83729ad3c5abc813a9d57e6f8
ttosi <ttosi519@gmail.com>
2016-08-16 22:01:41 +0000

    send email on address change

--- server.js
+++ server.js
@@ -14,12 +14,18 @@ LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
 NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
-
 var net = require('net'),
     http = require('http'),
     args = require('command-line-args'),
-    moment = require('moment'),
-	currentIp = 'N/A';
+    moment = require('moment');
+	
+var	formattedIp = 'Initializing',
+	currentIp = '';
+	
+var sendGrid = require('sendgrid')('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'),
+	sendGridHelper = require('sendgrid').mail,
+	email = new sendGridHelper.Email('ttosi519@gmail.com'),
+	subject = 'Home IP Address Changed';
     
 var options = args([
     { name: 'tcp-port', alias: 't', type: Number, defaultValue: 1337 },
@@ -29,12 +35,31 @@ var options = args([
 var server = net.createServer(function (socket) {
     socket.on('data', function (data) {
         var timestamp = moment(new Date()).format('MM-DD-YYYY hh:mm:ss A');
-        currentIp = data + ' (updated ' + timestamp + ')';
+		var formattedIp = data + ' (updated ' + timestamp + ')';
+		
+		if(data !== currentIp) {
+			var content = new sendGridHelper.Content('text/plain', 'New IP Address: ' + formattedIp);
+			var mail = new sendGridHelper.Mail(email, subject, email, content);
+			
+			var request = sendGrid.emptyRequest({
+				method: 'POST',
+				path: '/v3/mail/send',
+				body: mail.toJSON()
+			});
+
+			sendGrid.API(request, function(error, response) {
+				currentIp = data;
+				// console.log(response.statusCode)
+				// console.log(response.body)
+				// console.log(response.headers)
+			});
+		}
+		
     });
 }).listen(options['tcp-port']);
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Current IP Address: ' + currentIp);
+    res.end('Current IP Address: ' + formattedIp);
 }).listen(options['http-port']);
 
