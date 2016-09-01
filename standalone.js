57deb6b81a163d93a125f86b130bc5295261a7d1
ttosi <ttosi519@gmail.com>
2016-09-01 13:02:44 -0700

    .

--- standalone.js
+++ standalone.js
@@ -1,6 +1,6 @@
 /*
 MIT License
-Copyright (c) 2015, Tony S. Tosi <ttosi519@gmail.com>
+Copyright (c) 2016, Tony S. Tosi <ttosi519@gmail.com>
 Permission to use, copy, modify, and/or distribute this software
 for any purpose with or without fee is hereby granted, provided
 that the above copyright notice and this permission notice appear
---
+++ testpkg.js
@@ -0,0 +1,3 @@
+var adapter = require('./adapters/godaddy.js');
+
+adapter.run();
