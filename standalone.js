e140b7a2d557130d2a7d58d69c2f95c1172851b8
ttosi <ttosi519@gmail.com>
2016-08-31 19:00:20 -0700

    update license

--- standalone.js
+++ standalone.js
@@ -1,4 +1,5 @@
 /*
+MIT License
 Copyright (c) 2015, Tony S. Tosi <ttosi519@gmail.com>
 Permission to use, copy, modify, and/or distribute this software
 for any purpose with or without fee is hereby granted, provided
@@ -17,56 +18,48 @@ CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 require('dotenv').config();
 
-var net = require('net'),
-    publicIp = require('public-ip'),
-    args = require('command-line-args'),
-    moment = require('moment'),
+var net = require('net'), publicIp = require('public-ip'),
+    args = require('command-line-args'), moment = require('moment'),
     log = require('log4js').getLogger();
 
 var sendGrid = require('sendgrid')(process.env.SENDGRID_APIKEY),
-	sendGridHelper = require('sendgrid').mail,
-	email = new sendGridHelper.Email(process.env.EMAIL_TO),
-	subject = process.env.EMAIL_SUBJECT;
+    sendGridHelper = require('sendgrid').mail,
+    email = new sendGridHelper.Email(process.env.EMAIL_TO),
+    subject = process.env.EMAIL_SUBJECT;
 
-var	formattedIp = 'Initializing',
-	currentIp = '';
+var currentIp = undefined;
 
-var options = args([{
-    name: 'interval',
-    alias: 'i',
-    type: Number,
-    defaultValue: 5
-}]).parse();
+var options =
+    args([
+      {name: 'interval', alias : 'i', type: Number, defaultValue: 5}
+    ]).parse();
 
 log.info('starting ipupdater');
 log.info(options);
 
 function checkIp() {
-    publicIp(function(err, ip) {
-        if (err) {
-            log.error(err);
-            return;
-        }
+  publicIp(function(err, ip) {
+    if (err) {
+      log.error(err);
+      return;
+    }
 
-        if (ip !== currentIp) {
-            var timestamp = moment(new Date()).format('MM-DD-YYYY hh:mm:ss A');
-    		formattedIp = ip + ' (updated ' + timestamp + ')';
+    if (ip !== currentIp) {
+      var timestamp = moment(new Date()).format('MM-DD-YYYY hh:mm A');
+      var formattedIp = ip + ' (updated ' + timestamp + ')';
 
-            var content = new sendGridHelper.Content('text/plain', 'New IP Address: ' + formattedIp);
-			var mail = new sendGridHelper.Mail(email, subject, email, content);
+      var content = new sendGridHelper.Content('text/plain', 'New IP Address: ' + formattedIp);
+      var mail = new sendGridHelper.Mail(email, subject, email, content);
 
-			var request = sendGrid.emptyRequest({
-				method: 'POST',
-				path: '/v3/mail/send',
-				body: mail.toJSON()
-			});
+      var request = sendGrid.emptyRequest(
+          {method : 'POST', path : '/v3/mail/send', body : mail.toJSON()});
 
-			sendGrid.API(request, function(error, response) {
-				currentIp = ip;
-                log.info('IP address changed: ' + currentIp);
-			});
-        }
-    });
+      sendGrid.API(request, function(error, response) {
+        currentIp = ip;
+        log.info('IP address changed: ' + currentIp);
+      });
+    }
+  });
 }
 
 setInterval(checkIp, options['interval'] * 60000);
